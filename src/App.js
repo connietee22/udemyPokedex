6867f1b887f98fcd2ff99f7de4781cc35be7e02c
connietee22 <connietsangphotos@gmail.com>
2020-11-10 21:32:20 -0500

    styled header

--- src/App.js
+++ src/App.js
@@ -6,7 +6,11 @@ import './App.css';
 function App() {
   return (
     <div className="App wrapper">
-      <h1>Pokedex</h1>
+      <div className="headerContainer animate__animated animate__zoomInDown">
+        <a href="https://fontmeme.com/pokemon-font/">
+          <img src="https://fontmeme.com/permalink/201111/d28472afb9a391904f9eab67724b5431.png" alt="pokemon-font" border="0" className="pokedex"/>
+        </a>
+      </div>
       <Pokedex />
       {/* <Pokecard id={4} name="Charmander" type="fire" base_experience={62}/> */}
     </div>
